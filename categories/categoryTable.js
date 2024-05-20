441c99cc3a501b838bc7dc4be99e2b54bc39570d
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-05-20 10:12:13 -0300

    Criando model de tabela Category

---
+++ categories/categoryTable.js
@@ -0,0 +1,18 @@
+
+//Importando sequelize e conexão
+const sequelize = require('sequelize')
+const conection = require('../database/conection')
+
+//Criando nosso model de tabela 'Category'
+const Category = conection.define('categories',{
+    title: {
+        type: sequelize.STRING,
+        allowNull: false
+    }, slug: { //slug" é um termo usado para descrever uma versão amigável e legível de um nome ou título que é convertido em um formato adequado para URLs
+        type: sequelize.STRING,
+        allownull: false
+    }
+})
+
+//Exportando nosso Model de tabela
+module.exports = Category
