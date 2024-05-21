d4977a4e6d9c68ff70fb1a06a55bc32e2cd55e62
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-05-21 10:57:39 -0300

    Criando e renderizando nova rota com Router

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -3,12 +3,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/categories', (req, res) =>{
-    res.send('rota categorias')
-})
-
-router.get('/adm/categories/new', (req, res) =>{
-    res.send('rota de criação de categoria')
+router.get('/admin/categories/new', (req, res) =>{
+    res.render('admin/categories/new')
 })
 
 module.exports = router
