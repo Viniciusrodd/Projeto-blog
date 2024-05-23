cc13404bdc1c2a9b7ea18668eabcc5d31e9c389c
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-05-23 08:53:34 -0300

    Criando rota e instalando slugify

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -2,9 +2,25 @@
 
 const express = require('express')
 const router = express.Router()
+const categoryModel = require('./CategoryModel')
+const slugify = require('slugify')
 
 router.get('/admin/categories/new', (req, res) =>{
     res.render('admin/categories/new')
+})
+
+router.get('/categories/save', (req, res) =>{
+    var titleInput = req.body.title
+
+    categoryModel.create({
+        title: titleInput
+    })
+
+    if(titleInput != undefined){
+        
+    }else{
+        res.redirect('admin/categories/new')
+    }
 })
 
 module.exports = router
