8ea914bb02e0bd2c73bbbb0ea1de5a555581a94f
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-05-23 09:21:38 -0300

    Definindo conexão de tabela com dados enviados

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -9,15 +9,17 @@ router.get('/admin/categories/new', (req, res) =>{
     res.render('admin/categories/new')
 })
 
-router.get('/categories/save', (req, res) =>{
+router.post('/categories/save', (req, res) =>{
     var titleInput = req.body.title
 
-    categoryModel.create({
-        title: titleInput
-    })
-
     if(titleInput != undefined){
-        
+        categoryModel.create({
+            title: titleInput,
+            slug: slugify(titleInput) //Gera um slug (uma versão URL-amigável do título) a partir do título e o define na nova categoria.
+        }) 
+        .then(() =>{
+            res.redirect('/')
+        })
     }else{
         res.redirect('admin/categories/new')
     }
