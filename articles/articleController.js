189834c18cf9c8b51bf13664a29c5b49399daaea
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-05-30 11:27:11 -0300

    Introduzindo seleção de categorias na article view

--- articles/articleController.js
+++ articles/articleController.js
@@ -2,13 +2,19 @@
 
 const express = require('express')
 const router = express.Router()
+const categoryModel = require('../categories/CategoryModel')
 
 router.get('/article', (req, res) =>{
     res.send('rotas de artigo')
 })
 
 router.get('/admin/article/new', (req, res) =>{
-    res.render('admin/articlesEjs/newArticle')
+    categoryModel.findAll()
+        .then((categoryData) =>{
+            res.render('admin/articlesEjs/newArticle', {
+                dadosCategory: categoryData
+            })
+        })
 })
 
 module.exports = router
